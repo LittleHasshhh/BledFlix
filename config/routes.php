10d0075d48b6f03bb11d46e3ee29b45383acc84d
Stéphane LACHARME <stephlacharme@yahoo.fr>
2022-05-19 15:32:24 +0200

    toi

---
+++ config/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once '../src/controller/usersControls.php';
+
+
+switch ($uri) {
+    case '/':
+        $controller = new usersControls();
+        $controller->login();
+        break;
+    case '/create':
+        $controller = new usersControls();
+        $controller->create();
+        break;
+    case '/main':
+        $controller = new usersControls();
+        $controller->mainPage();
+        break;
+    case "/logOut":
+        $controller = new usersControls();
+        $controller->logOut();
+        break;
+    default:
+        //$controller = new errorControls();
+        //$controller->error404();
+        break;
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +8,74 @@
     <title>BledFlix</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="homeStyle.css">
+    <link rel="stylesheet" href="css/homeStyle.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+
+    
 </head>
+
 <body class="homePage">
     <header>
-        <nav>
-            <a href="index.php">
-            <img src="images/netflixLogo.png" alt="Logo Netflix">
-            </a>
+
+
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark lien">
+            <div class="flex ">    
+                <a href="index.php">
+                    <img src="images/netflixLogo.png" alt="Logo Netflix">
+                </a>
+                <a class="navbar-brand " href="#">Menu</a>
+                <div class="collapse navbar-collapse " id="navbarNavDarkDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                CATEGORIES
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Science Fiction</a></li>
+                                <li><a class="dropdown-item" href="#">Emotion</a></li>
+                                <li><a class="dropdown-item" href="#">Jeunesse</a></li>
+                                <li><a class="dropdown-item" href="#">Comedie</a></li>
+                                <li><a class="dropdown-item" href="#">Documentaire</a></li>
+                                <li><a class="dropdown-item" href="#">Drame</a></li>
+                                <li><a class="dropdown-item" href="#">Policier</a></li>
+                                <li><a class="dropdown-item" href="#">Horreur</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="input-group mb-3 space">
+                <span class="input-group-text" id="inputGroup-sizing-default">Recherche</span>
+                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            </div>
+            
+            <div class="collapse navbar-collapse compte " id="navbarNavDarkDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                               Utilisateur
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Compte</a></li>
+                                <li><a class="dropdown-item" href="#">Centre d'aide</a></li>
+                                <li><a class="dropdown-item" href="#">deconnecté</a></li>
+                                
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
         </nav>
+
+
+
+
     </header>
     <div class="tqt">
         <form action="">
@@ -39,6 +99,7 @@
         </form>
     </div>
 
-    zozoz
+
 </body>
+
 </html>
