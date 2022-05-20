a8552b1665d6f7b7e5c5f395b39c2d825479aeb9
Alexandre DA SILVA <alexandre71100@hotmail.com>
2022-05-20 13:33:17 +0200

    edit

---
+++ templates/edition.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bledflix</title>
+    <link rel="stylesheet" href="css/admin.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark lien">
+            <div class="flex ">
+                <a href="/main">
+                    <img src="images/bledflix.png" alt="Logo bledflix">
+                </a>
+                <a class="navbar-brand " href="/main">acceuil</a>
+                <a class="navbar-brand " href="/add">ajouter un film</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <h1>édition</h1>
+        <form action="">
+            <label for="">Titre</label>
+            <input type="text">
+            <label for="">Description</label>
+            <label for=""></label>
+        </form>
+
+    </main>
+</body>
+</body>
+
+</html>
